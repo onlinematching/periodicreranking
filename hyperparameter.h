f654ecbdb02b70e6e38a8ab476908ff6f4c1476d
shinnku <shinnkunikaidou@gmail.com>
2022-09-16 14:55:09 +0800

    init

---
+++ hyperparameter.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <array>
+#include <cctype>
+#include <cmath>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <type_traits>
+#include <vector>
+
+using f64 = double;
+using usize = std::uint32_t;
+using i64 = std::int64_t;
+using i32 = std::int32_t;
+
+constexpr f64 pi = 3.141592653589793;
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include "hyperparameter.h"
 
 int main(int argc, char *argv[]) {
     std::cout << "hello world!\n";
