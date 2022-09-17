622a78d47fb4e5879cf970dcd0c3e1a7b079fb37
shinnku <shinnkunikaidou@gmail.com>
2022-09-17 15:21:51 +0800

    tmp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,23 @@
+#include "hyperparameter.h"
 #include "util.hpp"
-#include <algorithm>
 #include <bits/ranges_algo.h>
 #include <boost/math/quadrature/gauss_kronrod.hpp>
 #include <boost/numeric/odeint.hpp>
-#include <limits>
-#include <mutex>
-#include <random>
-#include <thread>
-#include <type_traits>
 
-template <typename F1> f64 min_H(F1 H) {
+template <typename F1> f64 quick_min_H(F1 H, bool show_detail = false) {
   f64 min = std::numeric_limits<double>::max();
-  for (f64 z1 = 0; z1 < 1 + tol; z1 += step) {
-    // std::cout << z1 << std::endl;
-    for (f64 z2 = z1; z2 < 1 + tol; z2 += step) {
-      for (f64 y = 0; y < 1 + tol; y += step) {
-        f64 h = H(z1, z2, y);
-        if (h < min) {
-          min = h;
-          // std::cout << "z1 = " << z1 << " ;z2 = " << z2 << " ;y =  " << y
-          //           << "; min = " << min << std::endl;
+  f64 z1 = 0;
+  for (f64 z2 = z1; z2 < 1 + tol; z2 += precise) {
+    if (show_detail) {
+      std::cout << "z2 = " << z2 << std::endl;
+    }
+    for (f64 y = 0; y < 1 + tol; y += precise) {
+      f64 h = H(z1, z2, y);
+      if (h < min) {
+        min = h;
+        if (show_detail) {
+          std::cout << "z1 = " << z1 << " ;z2 = " << z2 << " ;y =  " << y
+                    << "; min = " << min << std::endl;
         }
       }
     }
@@ -27,16 +25,31 @@ template <typename F1> f64 min_H(F1 H) {
   return min;
 }
 
-f64 calculater(const A10 &a) {
-  auto g = [&a](f64 x) -> f64 {
-    if (x == 1.)
-      return 1;
-    const auto n = static_cast<usize>(x / step);
-    const f64 res = x - n * step;
-    const f64 h = a[n + 1] - a[n];
-    const f64 delta = h * res / step;
-    return a[n] + delta;
-  };
+template <typename F1> f64 min_H(F1 H, bool show_detail = false) {
+  f64 min = std::numeric_limits<double>::max();
+  f64 z1 = 0;
+  for (f64 z1 = 0; z1 < 1 + tol; z1 += step) {
+    if (show_detail) {
+      std::cout << z1 << std::endl;
+    }
+    for (f64 z2 = z1; z2 < 1 + tol; z2 += step) {
+      for (f64 y = 0; y < 1 + tol; y += step) {
+        f64 h = H(z1, z2, y);
+        if (h < min) {
+          min = h;
+          if (show_detail) {
+            std::cout << "z1 = " << z1 << " ;z2 = " << z2 << " ;y =  " << y
+                      << "; min = " << min << std::endl;
+          }
+        }
+      }
+    }
+  }
+  return min;
+}
+
+template <class F>
+f64 inner_calculater(F g, bool show_detail = false, bool use_quick = false) {
   auto G = [&g](f64 x) -> f64 {
     return boost::math::quadrature::gauss_kronrod<double, 23>::integrate(
         g, 0, x, 9, tol);
@@ -48,57 +61,66 @@ f64 calculater(const A10 &a) {
     f64 a4 = z1 * (1 - g(0));
     return a1 + a2 + a3 + a4;
   };
-  auto minnow = min_H(H);
-  return minnow;
+  if (use_quick) {
+    return quick_min_H(H, show_detail);
+  } else {
+    return min_H(H, show_detail);
+  }
 }
 
-template<class Axx>
-void print(Axx const &a) {
-  std::cout << "a = [";
-  for (auto x : a) {
-    std::cout << x << ", ";
-  }
-  std::cout << "]\n";
-}
-
-A10 a_generator(const A10 &a) {
-  static std::random_device rd;
-  static std::mt19937 gen(rd());
-  static std::uniform_real_distribution<double> dist(-step, step);
-  A10 a_next{};
-  a_next[a.size() - 1] = 1.;
-  for (usize i = 0; i < a.size() - 1; ++i) {
-    a_next[i] = a[i] + dist(gen);
-    if (a_next[i] < 0) {
-      a_next[i] = -a_next[i];
-    }
-    if (a_next[i] > 1) {
-      a_next[i] = 2 - a_next[i];
-    }
-  }
-  std::sort(a_next.begin(), a_next.end());
-  return a_next;
+f64 calculater(const A100 &a, bool show_detail = false,
+               bool use_quick = false) {
+  auto g = [&a](f64 x) -> f64 {
+    if (x >= 1.)
+      return 1;
+    const auto n = static_cast<usize>(x / step);
+    const f64 res = x - n * step;
+    const f64 h = a[n + 1] - a[n];
+    const f64 delta = h * res / step;
+    return a[n] + delta;
+  };
+  return inner_calculater(g, show_detail, use_quick);
 }
 
 int main(int argc, char *argv[]) {
-  print(next_generationa(a_end_10));
+  // print(next_generationa(a_end_10));
+  // return 0;
+  A100 a = g_pre_100;
+  f64 max = std::numeric_limits<f64>::min();
+  std::cout << calculater(a, true, true) << std::endl;
   return 0;
-  A10 a = a_init_10;
-  f64 max = std::numeric_limits<double>::min();
-  for (usize i = 0;; i++) {
-    if (i % 2000 == 0) {
-      std::cout << i << std::endl;
-    }
+  using namespace std::chrono_literals;
+  std::mutex mutex{};
+  usize n_core =
+      std::max(std::thread::hardware_concurrency() - 1, static_cast<usize>(1));
+  std::cout << "core num: " << n_core << std::endl;
+  std::this_thread::sleep_for(5000ms);
 
-    auto a_next = a_generator(a);
-    auto max_now = calculater(a_next);
-    if (max_now > max) {
-      max = max_now;
-      std::cout << i << std::endl;
-      std::cout << max << std::endl;
-      print(a_next);
-      std::cout << "-----------------------------" << std::endl;
-      a = a_next;
+  for (usize i = 0;;) {
+    std::vector<std::thread> threads{};
+    for (usize j = 0; j < n_core; j++) {
+      threads.push_back(std::thread([&]() {
+        auto a_next = a_generator(a);
+        auto max_now = calculater(a_next, false, true);
+
+        mutex.lock();
+        i++;
+        if (i % 400 == 0) {
+          std::cout << "i = " << i << std::endl;
+        }
+        if (max_now > max) {
+          max = max_now;
+          std::cout << "i = " << i << std::endl;
+          std::cout << "max = " << max << std::endl;
+          print(a_next);
+          std::cout << "-----------------------------" << std::endl;
+          a = a_next;
+        }
+        mutex.unlock();
+      }));
+    }
+    for (auto &thread : threads) {
+      thread.join();
     }
   }
   //
--- util.hpp
+++ util.hpp
@@ -1,11 +1,57 @@
 #include "hyperparameter.h"
 
+template <class Axx> Axx _a_generator(const Axx &a) {
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  static std::uniform_real_distribution<double> dist(-step, step);
+  Axx a_next{};
+  a_next[a.size() - 1] = 1.;
+  for (usize i = 0; i < a.size() - 1; ++i) {
+    a_next[i] = a[i] + dist(gen);
+    if (a_next[i] < 0) {
+      a_next[i] = -a_next[i];
+    }
+    if (a_next[i] > 1) {
+      a_next[i] = 2 - a_next[i];
+    }
+  }
+  std::sort(a_next.begin(), a_next.end());
+  return a_next;
+}
+
+inline A100 a_generator(const A100 &a) {
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  static std::uniform_real_distribution<f64> dist(-step, step);
+  static std::uniform_int_distribution<usize> dis_usize(0, 99);
+  A100 a_next = a;
+  usize i = dis_usize(gen);
+  a_next[i] = a[i] + dist(gen);
+  if (a_next[i] < 0) {
+    a_next[i] = -a_next[i];
+  }
+  if (a_next[i] > 1) {
+    a_next[i] = 2 - a_next[i];
+  }
+  std::sort(a_next.begin(), a_next.end());
+  return a_next;
+}
+
+template <class Axx> void print(Axx const &a) {
+  std::cout << "a = [";
+  for (auto x : a) {
+    std::cout << x << ", ";
+  }
+  std::cout << "]\n";
+}
+
 [[gnu::always_inline]] inline f64 _g(f64 x) {
   f64 Q = std::exp(beta * (x - 1));
   return Q;
 }
 
-inline A100 next_generationa(A10 a) {
+inline f64 array_g(f64 x) {
+  auto a = a_end_10;
   auto g = [&a](f64 x) -> f64 {
     if (x == 1.)
       return 1;
@@ -15,6 +61,10 @@ inline A100 next_generationa(A10 a) {
     const f64 delta = h * res / step;
     return a[n] + delta;
   };
+  return g(x);
+}
+
+template <class G> inline A100 next_generationa(G g) {
   A100 ans{};
   f64 next_step = 0.01;
   int i = 0;
